91ed4c2a120bbe551eaf1536c0272b4da981271c
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2020-03-18 22:41:37 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import './Components/Actions';
+import './Components/Home';
+import './Components/Reducers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+export default  App
